8b0b2776434780ee7a97ea72d1aa9e2a5ef00449
Goutam Dutta <duttagoutam@hotmail.com>
2021-01-16 14:11:09 +0530

    Baseline code
    Baseline code

---
+++ ConfigServer/src/test/java/com/example/demo/ConfigserverApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
