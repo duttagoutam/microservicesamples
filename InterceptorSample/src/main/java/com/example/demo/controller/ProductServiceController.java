2ebc0e45e7d53434acd7a9c3b25b527e904d5a4b
Goutam Dutta <duttagoutam@hotmail.com>
2021-01-20 22:06:55 +0530

    Base Code
    Base Code

--- InterceptorSample/src/main/java/com/example/demo/controller/ProductServiceController.java
+++ InterceptorSample/src/main/java/com/example/demo/controller/ProductServiceController.java
@@ -5,7 +5,10 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Product;
@@ -28,5 +31,25 @@ public class ProductServiceController {
 	@RequestMapping(value = "/products")
 	public ResponseEntity<Object> getProduct() {
 		return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
-	}	
+	}
+	
+	@RequestMapping(value = "/products", method = RequestMethod.POST)
+	public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+		productRepo.put(product.getId(), product);
+		return new ResponseEntity<>("Product is created successfully", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
+		product.setId(id);
+		productRepo.put(id, product);
+		return new ResponseEntity<>("Product is updated successfully", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Object> deleteProduct(@PathVariable("id") String id) {
+		productRepo.remove(id);
+		//productRepo.put(id, product);
+		return new ResponseEntity<>("Product is deleted successfully", HttpStatus.OK);
+	}		
 }
