01b322441a91de5cc7b3c380002e1f1d529696db
Goutam Dutta <duttagoutam@hotmail.com>
2021-01-21 22:04:01 +0530

    BaseCode
    BaseCode

---
+++ GoogleAuthSample/src/main/java/com/example/demo/GoogleauthsampleApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleauthsampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleauthsampleApplication.class, args);
+	}
+}
